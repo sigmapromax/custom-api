ac255211b84522f9b21ce996a2a232674de92d3a
Zhang Linfeng <zhanglinfengcs@outlook.com>
2023-11-27 20:45:40 +0800

    init project

---
+++ src/main/java/com/seed/customapi/CustomApiApplication.java
@@ -0,0 +1,13 @@
+package com.seed.customapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seed/customapi/CustomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seed.customapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
